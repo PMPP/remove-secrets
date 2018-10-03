d2e2df4d24b7ae3fbfe023f8d5925ad393765e33
Pedro Pereira <pedro.pereira@pmpp.pt>
2018-10-03 17:28:49 +0800

    Removing the secret

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,8 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://bananas:bananas123@ds121203.mlab.com:21203/mytest
+# This secret should be deleted
+# export MONGODB_CREDENTIALS=mongodb://bananas:bananas123@ds121203.mlab.com:21203/mytest
 
 # finally, run the app in dev mode...
 npm run dev
