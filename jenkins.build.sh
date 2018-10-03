37e04b5ec765d390002a7173c5110f00dd0be9b9
Pedro Pereira <pedro.pereira@pmpp.pt>
2018-10-03 17:21:49 +0800

    lol

--- jenkins.build.sh
+++ jenkins.build.sh
@@ -1,13 +1,16 @@
 # jenkins.build.sh
 # COPY THIS TO JENKINS BUILD STEP
 
-IMG_NAME=''  # Give your Docker image a name
-CTNR_NAME='' # Give your Docker container a unique name
+IMG_NAME='pmpp'  # Give your Docker image a name
+CTNR_NAME='pmpp' # Give your Docker container a unique name
 HOSTING_URL='app.thepingofhealth.com'
 
 # System a random port between 9001-9999
 PORT=$(shuf -i 9000-9999 -n 1)
 BACKUP_PORT=$(shuf -i 9000-9999 -n 1)
+
+# List
+docker ps
 
 # Clean up
 docker kill ${CTNR_NAME} || true
@@ -26,7 +29,7 @@ docker run \
   -p ${PORT}:3000 \
   --name ${CTNR_NAME} \
   ${IMG_NAME}
-  
+
 echo "Your app is hosted on: http://${HOSTING_URL}:${PORT}"
 
 sleep 1
