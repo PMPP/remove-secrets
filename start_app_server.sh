6223cb0f59418a1cf5b2981ab5455f73345f752d
Pedro Pereira <pedro.pereira@pmpp.pt>
2018-10-03 16:44:45 +0800

    Secret has been commited

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://{{USER}}:{{PASSWORD}}@ds666666.mlab.com:39999/some-database
+export MONGODB_CREDENTIALS= mongodb://triplejhacker:triplejhacker123password@ds121203.mlab.com:21203/mytest
 
 # finally, run the app in dev mode...
 npm run dev
